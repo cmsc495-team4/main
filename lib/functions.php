26cbb47fdc8f61d18a4c2adbbb1a467256c082b5
Ken Mabry <kwm997@gmail.com>
2019-06-12 19:57:28 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -73,7 +73,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-    require_once $_SERVER['DOCUMENT_ROOT'] . "/lib/dbconfig.php";
+    require $_SERVER['DOCUMENT_ROOT'] . "/lib/dbconfig.php";
     $litterExists = TRUE;
     
     	try {
