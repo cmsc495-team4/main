8eef683b26c8e071e780a1f87c5d03225b40b6bc
Ken Mabry <kwm997@gmail.com>
2019-06-12 20:09:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
     <?php getDropDown("generation_name", "generations"); ?>
     <hr>
     <?php
-		$testvar = checkLitterExists(1); 
+		$testvar = checkLitterExists(9); 
 		echo $testvar;  
     ?>
     
     <?php
- 		//addPups(10,4,"mouse","strain2", "2018-12-05") {
+ 		addPups(10,4,"mouse","strain2", "2018-12-05") {
     ?>
   </body>
 </html>
