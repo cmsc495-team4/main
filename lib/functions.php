0e003c4cdf4341cab2e2db2580a7825c052c64f8
Ken Mabry <kwm997@gmail.com>
2019-06-12 20:01:12 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -80,6 +80,7 @@ error_reporting(E_ALL);
 	        $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 			$query = "SELECT id FROM litters WHERE litterID = " . $litterID;
 			$result = $pdo->query($query);
+			$result->setFetchMode(PDO::FETCH_ASSOC);
 
 			if ($result->num_rows == 0) {
 				$litterExists = FALSE;
@@ -116,6 +117,8 @@ error_reporting(E_ALL);
 			}
 			
 			$result = $pdo->query($query);
+			$result->setFetchMode(PDO::FETCH_ASSOC);
+
 		}
 		else {
 			echo "Error - Duplicate Litter ID";
