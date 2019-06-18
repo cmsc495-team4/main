60e34d2ac10558c3375e8f85da733f390b7437ca
Ken Mabry <kwm997@gmail.com>
2019-06-17 20:00:48 -0400

    updates

--- mainpage.php
+++ mainpage.php
@@ -15,7 +15,7 @@ require $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
 
 
   <div>
-  <img align="left" src="img/mouse-1.png" height="117" width="87">
+  <img align="left" padding-top="5px" src="img/mouse-1.png" height="117" width="87">
     <form>
     <fieldset>
     <legend>Filter Results</legend>
