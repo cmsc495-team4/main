da9156df7fee11eafc8c9cf8b06a89fdd58995ff
Ken Mabry <kwm997@gmail.com>
2019-06-19 05:44:12 -0400

    Update functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -345,7 +345,11 @@ function displayAnimalTable()
     echo "<form>\n";
     echo "<table id=\"maintable\" class=\"display compact\">\n";
     echo "<thead>";
-    echo "<tr><th>Select</th><th>ID</th><th>Investigator</th><th>Tag Number</th><th>Species</th><th>Class</th><th>Sex</th><th>Strain</th><th>Genotype</th><th>Litter ID</th><th>Parent Pair</th><th>Birth Date</th><th>Wean Date</th><th>Tag Date</th><th>Deceased</th><th>Transferred</th><tr class=\"animals\">\n";
+    echo "<tr><th>Select</th> <th>ID</th> <th>Investigator</th> <th>Tag Number</th> 
+    		<th>Species</th> <th>Class</th> <th>Sex</th> <th>Strain</th> <th>Genotype</th> 
+    		<th>Litter ID</th> <th>Parent Pair</th> <th>Birth Date</th> <th>Wean Date</th> 
+    		<th>Tag Date</th> <th>Deceased</th> <th>Transferred</th> 
+    		<\tr>\n";
 	echo "</thead>
 	<tbody>";
 
