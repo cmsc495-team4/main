14856eb79ecd277195ba2414bb5ab745854b76c3
Tiffany Matthew <tiffanymatth@gmail.com>
2019-06-24 23:20:45 -0400

    Update addLitter.php
    Add script from Jquery to toggle add to existing litter dropdown and label if new litter checkbox is selected.

--- addLitter.php
+++ addLitter.php
@@ -11,6 +11,17 @@
   error_reporting(E_ALL);
   require $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
   ?>
+
+	<!--script to toggle add to existing dropdown: will disappear if new litter box is checked-->
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function(){
+			$('input[type="checkbox"]').click(function(){
+			var inputValue = $(this).attr("value");
+			$("." + inputValue).toggle();
+		});
+	});</script>
+
 </header>
 
 <body>
@@ -20,35 +31,46 @@
 			<table>
         <tr>
           <!-- <input type="hidden" name="newLitter" value="0" > ?php echo $newLitter; ?>-->
-           <td><input type="checkbox" name="newLitter">New Litter</td>
+           <td>
+						 	<div>
+           			<input type="checkbox" name="newLitter" value="newLitter">New Litter
+           		</div>
+					 </td>
         </tr>
           <tr>
             <td>Pair:</td>
   					<td><select name="pairID">
             	  <?php getDropDown("pairID", "breeding_pairs", $breedingPair); ?>
-  		         </select></td>
-               <td>Add to existing Litter:</td>
-     					<td><select name="litterID">
-               	  <?php getDropDown("litterID", "litters", $litterID); ?>
-     			        </select></td>
+							</select></td>
+     					<td>
+								<div class="newLitter">
+									<label for="litterID">Add to existing Litter:</label>
+									<select name="litterID">
+	               	  <?php getDropDown("litterID", "litters", $litterID); ?>
+	     			        </select>
+								</div>
+							</td>
              </tr>
              <tr>
                <!--?php echo $dob; ?>-->
-               <td>Date of Birth:</td>
-     					<td><input type="date" name="birth_date"></td>
-              <td>Number of pups:</td>
-             <td><input type="text" name="numPups"></td>
-             </tr>
+               	<td>Date of Birth:</td>
+     						<td><input type="date" name="birth_date"></td>
+							</tr>
+							<tr>
+								<td>Number of pups:</td>
+							 	<td><input type="text" name="numPups"></td>
+							</tr>
              <tr>
                <td>Comments:</td>
        				<td>
        					<textarea id="commentBox" name="commentBox" rows="5" cols="33"></textarea>
        				</td>
-              <td><button class="button" type="submit" name="add">Add</button></td>
+ 							</tr>
+							<tr>
+								<td><button class="button" type="submit" name="add">Add</button></td>
+							</tr>
 
-             </tr>
+          </form>
 
-            	</form>
-
-            </body>
+      </body>
 </html>
