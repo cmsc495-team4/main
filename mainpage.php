efc9f3ad052649cbefa256ef8b9f4a56fb2d67f4
Ken Mabry <kwm997@gmail.com>
2019-06-24 22:35:39 -0400

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -114,7 +114,7 @@ if (isset($_REQUEST["clear"])) {
 <link rel="stylesheet"
 	href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<!-- <script> -->
+<script> 
 //   $( function() {
 //     $( document ).tooltip({
 //     	position: { my: "center top-120%",  at: "right top",collision: "fitflip"}
@@ -123,7 +123,7 @@ if (isset($_REQUEST["clear"])) {
 //     	});
     	    
 //   } );
-<!--    </script> -->
+ </script> 
 
 
 
