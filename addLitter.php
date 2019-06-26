d3e841250314daf1eb16ba8f5241dd1faa2bed1a
Tiffany Matthew <tiffanymatth@gmail.com>
2019-06-25 21:07:27 -0400

    Update addLitter.php

--- addLitter.php
+++ addLitter.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>Add Litter</title>
-	<link rel="stylesheet" type="text/css" href="addPupsStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/addPupsStyle.css">
 </head>
 
 <header>
@@ -20,57 +20,51 @@
 			var inputValue = $(this).attr("value");
 			$("." + inputValue).toggle();
 		});
-	});</script>
+		});</script>
 
 </header>
 
 <body>
   <form action="<?php $_SERVER['REQUEST_URI']?>" method="POST">
 		<fieldset>
-			<legend>Add Litter Form</legend>
-			<table>
-        <tr>
-          <!-- <input type="hidden" name="newLitter" value="0" > ?php echo $newLitter; ?>-->
-           <td>
-						 	<div>
+		<legend>Add Litter Form</legend>
+		<table>
+        		<tr>
+          	<!-- <input type="hidden" name="newLitter" value="0" > ?php echo $newLitter; ?>-->
+          		 <td>
+				<div>
            			<input type="checkbox" name="newLitter" value="newLitter">New Litter
-           		</div>
-					 </td>
-        </tr>
-          <tr>
-            <td>Pair:</td>
-  					<td><select name="pairID">
-            	  <?php getDropDown("pairID", "breeding_pairs", $breedingPair); ?>
-							</select></td>
-     					<td>
-								<div class="newLitter">
-									<label for="litterID">Add to existing Litter:</label>
-									<select name="litterID">
-	               	  <?php getDropDown("litterID", "litters", $litterID); ?>
-	     			        </select>
-								</div>
-							</td>
-             </tr>
-             <tr>
-               <!--?php echo $dob; ?>-->
-               	<td>Date of Birth:</td>
-     						<td><input type="date" name="birth_date"></td>
-							</tr>
-							<tr>
-								<td>Number of pups:</td>
-							 	<td><input type="text" name="numPups"></td>
-							</tr>
-             <tr>
-               <td>Comments:</td>
-       				<td>
-       					<textarea id="commentBox" name="commentBox" rows="5" cols="33"></textarea>
-       				</td>
- 							</tr>
-							<tr>
-								<td><button class="button" type="submit" name="add">Add</button></td>
-							</tr>
+           			</div>
+			 </td>
+			 <td>
+				 <div class="newLitter">
+					<label for="litterID">Add to existing Litter:</label>
+					<select name="litterID"><?php getDropDown("litterID", "litters", $litterID); ?>
+					</select>
+				</div>
+			</td>
+       		 </tr>
+         	 <tr>
+            		<td>Pair:</td>
+  			<td><select name="pairID">
+            	  		<?php getDropDown("pairID", "breeding_pairs", $breedingPair); ?></select></td>
+             	</tr>
+            	 <tr>
+               		<!--?php echo $dob; ?>-->
+               		<td>Date of Birth:</td>
+     			<td><input type="date" name="birth_date"></td>
+		</tr>
+		<tr>
+			<td>Number of pups:</td>
+			<td><input type="text" name="numPups"></td>
+		</tr>
+             	<tr>
+              		<td><label for="commentBox">Comments:</label></td>
+       			<td><textarea id="commentBox" name="commentBox" rows="5" cols="33"></textarea></td>
+ 		</tr>
+		<tr>
+		<td><button class="button" type="submit" name="add">Add</button></td></tr>
+	</form>
 
-          </form>
-
-      </body>
+     </body>
 </html>
