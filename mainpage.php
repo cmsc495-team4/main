c002d08ac5452bdb81c4f7fabd1ffd1ef4277978
Ken Mabry <kwm997@gmail.com>
2019-06-26 20:01:07 -0400

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -253,13 +253,13 @@ $(document).ready( function () {
         $(this).css('background-color','#ffa'); 
      });
      $('table.display tr.even').mouseout(function(){
-        $(this).css('background-color','#fafafa'); 
+        $(this).css('background-color','#ffffff'); 
      }); 
      $('table.display tr.odd').hover(function(){
          $(this).css('background-color','#ffa'); 
       });
       $('table.display tr.odd').mouseout(function(){
-         $(this).css('background-color','#ddd'); 
+         $(this).css('background-color','#f9f9f9'); 
       }); 
 
 //   //assume newW is a calculated value that I produce with every resize, i.e., the 'available width'
