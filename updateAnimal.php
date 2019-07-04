8e3175d0b9f249de0c9242fb2b05ceea6057d246
JacHat <42483231+JacHat@users.noreply.github.com>
2019-07-04 10:52:37 -0400

    Add files via upload

---
+++ updateAnimal.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/addBrPairStyle.css">
+	<link rel="stylesheet" type="text/css" href="css/mainPageStyle.css">
+
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script type="text/javascript">
+		$(document).ready(function(){
+		//strains dropdown
+		$("[name='pi_name']").change(function(){
+			var pi_name = $(this).val();
+			var fieldTable = ['strain_name', 'strains, pi_authorized_strains', ''];
+			var conditions = ['authPI_username', '\'' + pi_name + '\'', 'authPI_strain_ID', 'id_strain'];
+			$.ajax({
+				type: "POST",
+				url: "/lib/ajaxDropdown.php",
+				data: {fieldTable: fieldTable, conditions: conditions},
+				cache: false,
+				success: function(strains){
+					$("[name='strain_name']").html(strains);
+				}
+			});	
+		});
+		
+		});
+	</script>	
+	
+</head>
+
+<header>
+	<h1>Add New Animal Form</h1>
+	<?php
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+		require $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
+		
+		$message = "";
+		$change_tag = "";
+		if(isset($_POST['getAnimal'])){
+			$change_tag = $_POST['tagNumber'];
+			displayAnimalTable();
+			$message = "All updates will affect animal with Tag #: " . $change_tag;
+		}
+		
+		if(isset($_POST['update'])){
+			//check that a tag number was selected before attempting update
+			if(empty($_POST['change_tag'])){
+				$message = "Select animals current Tag # before updating";
+			}else{
+			var_dump($_POST);
+			//$change_tag = $_POST['change_tag'];
+			var_dump($change_tag);
+			$message = "Select Tag # to update" ;
+				updateAnimal();
+			}
+		}
+	?>
+	
+
+	
+<div style="height: auto; width: auto;">
+			<form class="header-form" action="<?php $_SERVER['REQUEST_URI']?>"
+				method="POST">
+				<fieldset>
+				<table style="margin: auto">
+					<legend>Filter Results</legend>
+						<tr>
+							<td>Tag#:</td>
+							<td><select name="tagNumber"> 
+          	  <?php getDropDown("tagNumber", "animals", $change_tag); ?>
+			</select></td>
+
+							<td class="filter-button"><button class="action" type="submit"
+									name="getAnimal" value="submitted">Get Current Data</button></td>
+						</tr>
+					</table>
+				</fieldset>
+			</form>
+		</div>
+		
+	</header>
+
+<body>
+	<form action="<?php $_SERVER['REQUEST_URI']?>" method="POST">
+	<div>
+		<?php 
+			if(isset($message)){
+				echo '<label class="text-danger" style="color:red; text-align:center; font-size:1.5em;">' . $message . '</label>';
+			}
+		?>
+		<input type="hidden" name="change_tag" value="<?php echo $change_tag ?>">
+		<div style="text-align: center">
+			<input type="radio" id="pup" name="classification" value="pup">
+				<label for="pup">Pup</label>
+			<input type="radio" id="weanling" name="classification" value="weanling">
+				<label for="weanling">Weanling</label>
+			<input type="radio" id="breeder" name="classification" value="breeder">
+				<label for="breeder">Breeder</label>
+		</div>
+		<table class="table1">
+			<tr>
+				<td></td>	
+			</tr>
+			<tr>
+				<td>PI:</td>
+				<td>
+					<select name="pi">
+						<?php getInvestigators(""); ?>
+					</select>
+				</td>
+				<td>Species:</td>
+				<td>
+					<select name="species_name">
+						<?php getDropDown("species_name", "animals", $species_name); ?>
+					</select>
+				</td>
+				<td>Strain: ([Re]select PI first)</td>
+				<td>
+					<select name="strain_name">
+						<option>Select PI</option> <!-- ajax created based on selected PI -->
+					</select>
+				</td>
+			</tr>
+			<tr>
+				
+				<td>DOB:</td>
+				<td>
+					<input type="date" name="dob" placeholder="mm/dd/yyyy">
+				</td>
+				
+				<td>Wean Date:</td>
+				<td>
+					<input type="date" name="weanDate" placeholder="mm/dd/yyyy">
+				</td>
+				
+				<td>Location:</td>
+				<td>
+					<select name="location">
+						<?php getDropDown("location_name", "location", ""); ?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Tag #:</td>
+				<td>
+					<input type="text" name="tagNum" placeholder="Tag #">
+				</td>
+				<td>Tag Date:</td>
+				<td>
+					<input type="date" name="tagDate" placeholder="Tag Date">
+				</td>
+				<td>Sex:</td>
+				<td>
+					<select name="sex">
+						<option value="">-select-</option>
+						<option value="Male">Male</option>
+						<option value="Female">Female</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>Genotype</td>
+				<td>
+					<select name="genotype">
+						<?php getDropDown("genotype_name", "genotypes", "") ?>
+					</select>
+				</td>
+				<td>Litter ID</td>
+				<td>
+					<select name="litter">
+						<?php getDropDown("litterID", "litters", "") ?>
+					</select>
+				</td>
+				<td>
+					<input type="checkbox" name="transfer">
+					<label for="transfer">Transferred</label>
+					<input type="checkbox" name="deceased" value="true">
+					<label for="deceased">Deceased</label>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div>
+		<table class="table3">
+			<tr>
+				<td>Comments:</td>
+				<td>
+					<textarea id="commentBox" name="commentBox" rows="5" cols="33"></textarea>
+				</td>
+		</table>
+	</div>
+	<div class="buttonDiv">
+		<button class="button" type="submit" name="update">Update</button>
+	</div>
+	</form>
+</body>
+</html>
