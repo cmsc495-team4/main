bd2ebd3c3fbbafa821964f250b9465e6a1c103c5
Tiffany Matthew <tiffanymatth@gmail.com>
2019-07-06 21:11:08 -0400

    Update addLitter.php

--- addLitter.php
+++ addLitter.php
@@ -29,55 +29,62 @@
 		<fieldset>
 			<legend>Add Litter Form</legend>
 			<table>
-        		<tr>
-           			<td>
-				<div>
-           			<input type="checkbox" name="newLitter" value="newLitter">New Litter
-					</div>
-				 </td>
-				 <td>
-					<div class="newLitter">
-					<label for="litterID">Add to existing Litter:</label>
-					<select name="litterID">
-					<?php getDropDown("litterID", "litters", $litterID); ?>
-					 </select>
-					</div>
+        <tr>
+           <td>
+						 	<div>
+           			<input type="checkbox" name="newLitterID" value="newLitter">New Litter
+           		</div>
 					 </td>
-       			 </tr>
-         		 <tr>
-           		 <td>Pair:</td>
-  			<td><select name="pairID">
-            	  		<?php getDropDown("pairID", "breeding_pairs", $breedingPair); ?>
+					 <td>
+						 <div class="newLitter">
+							 <label for="litterID">Add to existing Litter:</label>
+							 <select name="litterID">
+								 <?php getDropDown("litterID", "litters", $litterID); ?>
+								 </select>
+						 </div>
+					 </td>
+        </tr>
+          <tr>
+            <td>Pair:</td>
+  					<td><select name="pairID" required>
+            	  <?php getDropDown("pairID", "breeding_pairs", $breedingPair);
+								?>
 							</select></td>
-             		</tr>
-             		<tr>
-               			<td>Date of Birth:</td>
-     				<td><input type="date" name="birth_date"></td>
-			</tr>
-			<tr>
-				<td>Number of pups:</td>
-				<td><input type="text" name="numPups"></td>
-			</tr>
-            		 <tr>
-              			 <td><label for="commentBox">Comments:</label></td>
+             </tr>
+             <tr>
+               	<td>Date of Birth:</td>
+     						<td><input type="date" name="birth_date" required></td>
+							</tr>
+							<tr>
+								<td>Number of pups:</td>
+							 	<td><input type="text" name="numPups" required></td>
+
+							</tr>
+             <tr>
+               <td><label for="commentBox">Comments:</label></td>
        				<td>
        					<textarea id="commentBox" name="commentBox" rows="5" cols="33"></textarea>
        				</td>
- 			</tr>
-			<tr>
-				<td><button class="button" type="submit" name="add">Add</button></td>
-			</tr>
-			<?php
-// 				if (isset($_POST["add"])) { //if add button is clicked
-// 				//If newLitter check box is checked call newLitterIncrement from functions.php to create newLitterID
-// 					if (isset($_POST["newLitterID"])) {
-// 					$litterID = newLitterIncrement();
-// 					createNewLitter($litterID); //creates new litter with null values
-// 					}else {
-								
-// 					}
-// 				}
-			?>				
-		</form>						
+ 							</tr>
+							<tr>
+								<td><button class="button" type="submit" name="add">Add</button></td>
+
+								<?php
+								if (isset($_POST["add"])) { //if add button is clicked
+									//If newLitter check box is checked call newLitterIncrement from functions.php to create newLitterID
+									if (isset($_POST["newLitterID"])) {
+										$litterID = newLitterIncrement();
+									}
+										$numPups = $_POST['numPups'];
+										$dob = $_POST['birth_date'];
+										$comments = $_POST['commentBox']
+										addPups($litterID,$breedingPair, $numPups, $dob,$comments);
+
+								}
+								?>
+							</tr>
+
+          </form>
+
       </body>
 </html>
