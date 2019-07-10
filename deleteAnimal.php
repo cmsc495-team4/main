46a0c6b6617d0af995ae1fa36680f9db75dde14d
Tiffany Matthew <tiffanymatth@gmail.com>
2019-07-09 21:12:59 -0400

    Add files via upload

---
+++ deleteAnimal.php
@@ -0,0 +1,9 @@
+<?php
+//recieves animal id from selected value from mainpage. AJAX method used to send data
+	$selectedValue = (int)$_POST['selectedValue'];
+
+  require $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
+
+	deleteAnimal($selectedValue);
+
+ ?>
