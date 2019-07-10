bb83eba40f6791b131146294c2ba330e9f63968a
Ken Mabry <kwm997@gmail.com>
2019-07-09 23:07:49 -0400

    Update mainpage.php

--- mainpage.php
+++ mainpage.php
@@ -253,7 +253,7 @@ require $_SERVER['DOCUMENT_ROOT'] . "/lib/functions.php";
 	<script>
 $(document).ready( function () {
     $('#maintable').DataTable({
-    		responsive: true;
+    		responsive: true,
     		order: [[ 2, 'asc' ], [ 1, 'asc' ]]
 	});
     $('table.display tr.even').hover(function(){
