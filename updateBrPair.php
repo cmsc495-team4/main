358f8c40010d99724c6c344222330fff3fc4da74
JacHat <42483231+JacHat@users.noreply.github.com>
2019-07-10 21:17:38 -0400

    Update updateBrPair.php

--- updateBrPair.php
+++ updateBrPair.php
@@ -71,7 +71,7 @@
 		$("[name='species_name']").change(function(){
 			var pi_name = $("[name='pi_name']").val();
 			var species_name = $(this).val();
-			var fieldTable = ['animalID', 'animals, PI_assigned_animals', ''];
+			var fieldTable = ['tagNumber', 'animals, PI_assigned_animals', ''];
 			var conditions = ['animalID', 'PI_animalID', 'PI_username', '\'' + pi_name + '\'', 'species_name', '\'' + species_name + '\'',
 			'classification', '\'breeder\'', 'sex', '\'Male\''];
 			$.ajax({
@@ -89,7 +89,7 @@
 		$("[name='species_name']").change(function(){
 			var pi_name = $("[name='pi_name']").val();
 			var species_name = $(this).val();
-			var fieldTable = ['animalID', 'animals, PI_assigned_animals', ''];
+			var fieldTable = ['tagNumber', 'animals, PI_assigned_animals', ''];
 			var conditions = ['animalID', 'PI_animalID', 'PI_username', '\'' + pi_name + '\'', 'species_name', '\'' + species_name + '\'',
 			'classification', '\'breeder\'', 'sex', '\'Female\''];
 			$.ajax({
