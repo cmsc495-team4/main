8bced9310c881b8df21b9bc143bf089f78887831
Ken Mabry <kwm997@gmail.com>
2019-07-11 02:10:35 -0400

    Update addNewAnimal.php

--- addNewAnimal.php
+++ addNewAnimal.php
@@ -14,14 +14,6 @@
 	<title>RITA - Add Animal</title>
 	<link rel="stylesheet" type="text/css" href="css/mainPageStyle.css">
 	<link rel="stylesheet" type="text/css" href="css/userDropdown.css">
-	<div class="main-body">
-	<center>
-	<div class="logo">
-		<img align="center" class="ritalogo" src="img/ritalogo-1.png"
-			height="97" width="360">
-		</br></br>
-		<h2 class="maintitle">Add New Animal Form
-		<br style = "line-height:100px;"></br></div>
 			
 	
 	
